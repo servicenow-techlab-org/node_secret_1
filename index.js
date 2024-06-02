d8db5e90c75e0ec6f570bd9c78f4a7ce74da53c0
sandySN <140835577+sandySN@users.noreply.github.com>
2024-06-03 01:36:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const rl = readline.createInterface({
 });
 
 const configuration = new Configuration({
-    apiKey: "23487417419837183198",
+    apiKey: "sk-UqwyX05Wi6jPZBf8gRdvT3BlbkFJVWlry4I5bM3TdkIUl5121",
 });
 
 const openai = new OpenAIApi(configuration);
